da1db11724dd10ca3401ec73b1523840c31394b0
Roy Sheppard <rsheppard83@gmail.com>
2023-10-03 20:41:07 +0100

    init commit

---
+++ src/assets/images.ts
@@ -0,0 +1,26 @@
+const imageWidth = '1200';
+
+const images = [
+	{
+		title: 'Funny Monkey',
+		url: `https://images.unsplash.com/photo-1506126944674-00c6c192e0a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=${imageWidth}&q=80`,
+	},
+	{
+		title: 'Funny Horse',
+		url: `https://images.unsplash.com/photo-1551884831-bbf3cdc6469e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=${imageWidth}&q=80`,
+	},
+	{
+		title: 'Funny Giraffe',
+		url: `https://images.unsplash.com/photo-1593871075120-982e042088d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=${imageWidth}&q=80`,
+	},
+	{
+		title: 'Funny Cow',
+		url: `https://images.unsplash.com/photo-1556997685-309989c1aa82?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=${imageWidth}&q=80`,
+	},
+	{
+		title: 'Funny Bear',
+		url: `https://images.unsplash.com/photo-1623584973952-182bcb43b8ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=${imageWidth}&q=80`,
+	},
+];
+
+export default images;
